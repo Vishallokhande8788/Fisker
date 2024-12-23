720eee07547de4cbdfd7120cd1e2a094a41ff184
vishal lokhande <lvishal108@gmail.com>
2024-12-23 14:30:48 +0000

    bun , next js setup

---
+++ app/about/page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'hello world',
+  description: 'Woking in Next js',
+};
+function HomePage() {
+  return (
+    <html>
+        <body>
+            <h1>page 2</h1>
+        </body>
+    </html>
+  );
+}
+
+export default HomePage;
---
+++ app/page.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export const metadata = {
+  title: 'hello world',
+  description: 'Woking in Next js',
+};
+function HomePage() {
+  return (
+    <html>
+        <body>
+            <h1>page 1</h1>
+        </body>
+    </html>
+  );
+}
+
+export default HomePage;
