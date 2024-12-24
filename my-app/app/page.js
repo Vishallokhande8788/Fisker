6e53fc55447a07b04418ec6f208959e0a5ccab0e
vishal lokhande <lvishal108@gmail.com>
2024-12-24 14:22:17 +0000

    Add Header

---
+++ my-app/app/page.js
@@ -0,0 +1,17 @@
+"use client";
+import Header from "@/components/Header";
+import Preview from "@/components/Preview";
+import Configurator from "@/components/Configurator";
+
+function Home() {
+  return (
+    <div onContextMenu={(e) => e.preventDefault()} className="min-h-screen">
+      <Header />
+      <main 
+      <Preview />
+      <Configurator />
+    </div>
+  );
+}
+
+export default Home;
